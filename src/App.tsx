db8915516a289948200ff44d917b70c6b9efd941
a000906 <werther0704@gmail.com>
2026-06-01 16:15:58 +0900

    fix: set BrowserRouter basename from Vite BASE_URL

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function Home() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
